c3c6b2a07e688ba02436cbdff95adff762838946
Daniel Britten <daniel.britten@outlook.com>
2020-09-12 15:08:38 +1200

    Added required files for Nix usage

---
+++ nix/default.nix
@@ -0,0 +1,15 @@
+# paramaterised derivation with dependencies injected (callPackage style)
+{ pkgs, stdenv, opam2nix }:
+let
+	args = {
+		inherit (pkgs.ocaml-ng.ocamlPackages_4_08) ocaml;
+		selection = ./opam-selection.nix;
+		src = ../.;
+	};
+	opam-selection = opam2nix.build args;
+	resolve = opam2nix.resolve args [ "archetype.opam" ];
+in
+{
+	inherit (opam-selection) archetype;
+	inherit resolve;
+}
---
+++ opam2nix.nix
@@ -0,0 +1,5 @@
+# In your project, you should use:
+import (builtins.fetchTarball "https://github.com/timbertson/opam2nix/archive/version-1.1.0.tar.gz") {}
+
+# For easier development, these examples reference the local expression directly
+# import ../default.nix {}
