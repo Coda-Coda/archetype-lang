edb2d1c73df8a79bdbad22a390aa0607f6f876fa
Daniel Britten <Coda-Coda@users.noreply.github.com>
2020-10-12 13:45:49 +1300

    Shifted to a custom version of opam2nix

--- opam2nix.nix
+++ opam2nix.nix
@@ -1,5 +1,5 @@
 # In your project, you should use:
-import (builtins.fetchTarball "https://github.com/timbertson/opam2nix/archive/version-1.2.0.tar.gz") {}
+import (builtins.fetchTarball "https://github.com/Coda-Coda/opam2nix/archive/c587ec9004221d6f23907b09c639094950196797.tar.gz") {}
 
 # For easier development, these examples reference the local expression directly
 # import ../default.nix {}
