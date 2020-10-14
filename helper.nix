549b085bc765b6e74763fb0b3c41a36c55917369
Daniel Britten <Coda-Coda@users.noreply.github.com>
2020-10-14 00:24:30 +0000

    Revert "Changed to pinned version of nixpkgs-unstable"
    Pinning did not work.
    
    This reverts commit 99fd7ec3a326f4feabcbb6ecc763aae316731e9b.

--- helper.nix
+++ helper.nix
@@ -1,3 +1,3 @@
 # standalone derivation, for nix-build, nix-shell, etc
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/89281dd1dfed6839610f0ccad0c0e493606168fe.tar.gz") {}, opam2nix ? import ./opam2nix.nix }:
+{ pkgs ? import <nixpkgs> {}, opam2nix ? import ./opam2nix.nix }:
 pkgs.callPackage ./nix { inherit opam2nix; }
